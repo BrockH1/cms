c8a9dc5a11b73b92bae2088d5f8850ef712ad60f
BrockH1 <hoskinsbe@gmail.com>
2025-09-27 12:17:08 -0600

    created contacts list and contacts detail components

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,10 +2,18 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
+import { Contacts } from './contacts/contacts';
+import { ContactDetail } from './contacts/contact-detail/contact-detail';
+import { ContactList } from './contacts/contact-list/contact-list';
+import { Header } from './header/header';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Contacts,
+    ContactDetail,
+    ContactList,
+    Header
   ],
   imports: [
     BrowserModule
